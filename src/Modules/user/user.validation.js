4559f289669791417ee99d74fe347f05bd8a0ac1
ahmed1x0 <ahmibrahim071@gmail.com>
2025-08-14 15:14:21 +0200

    Update user.validation.js -- optional2

--- src/Modules/user/user.validation.js
+++ src/Modules/user/user.validation.js
@@ -1,14 +1,14 @@
 import Joi from "joi";
 
 export const updateProfileSchema = Joi.object({
-  firstName: Joi.string().min(3).max(50).optional().empty(""),
-  lastName: Joi.string().min(3).max(50).optional().empty(""),
-  email: Joi.string().email().optional().empty(""),
+  firstName: Joi.string().trim().min(3).max(50).optional().empty(""),
+  lastName: Joi.string().trim().min(3).max(50).optional().empty(""),
+  email: Joi.string().trim().email().optional().empty(""),
 
-  oldPassword: Joi.string().min(6).optional().empty(""),
+  oldPassword: Joi.string().trim().min(6).optional().empty(""),
   newPassword: Joi.when("oldPassword", {
     is: Joi.exist(),
-    then: Joi.string().min(6).required().messages({
+    then: Joi.string().trim().min(6).required().messages({
       "any.required": "New password is required when old password is provided",
     }),
     otherwise: Joi.forbidden(),
